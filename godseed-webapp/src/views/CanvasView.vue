78ffed86eda4d554e7b1e2c9204c8fd0e9d38ca8
lenmoore <len.vainmaa@gmail.com>
2024-07-15 15:34:22 +0300

    doesn't really work that well

--- godseed-webapp/src/views/CanvasView.vue
+++ godseed-webapp/src/views/CanvasView.vue
@@ -1,62 +1,72 @@
 <template>
-    <div>
-        <h1>Drag and Drop Example</h1>
+    <div class="canvas">
         <div
-            v-for="(item, index) in items"
-            :key="item.id"
-            class="draggable"
+            v-for="(box, index) in boxes"
+            :key="box.id"
+            class="bounding-box"
             :draggable="true"
-            @dragstart="onDragStart(index)"
+            @dragstart="(event) => onDragStart(event, index)"
             @dragover.prevent
-            @drop="onDrop(index)"
+            @drop="(event) => onDrop(event, index)"
+            :style="{ left: box.x + 'px', top: box.y + 'px', width: box.width + 'px', height: box.height + 'px' }"
         >
-            {{ item.name }}
+            {{ box.content }}
         </div>
     </div>
 </template>
-
 <script>
 import { draggable, dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
 
 export default {
     data() {
         return {
-            items: [
-                { id: 1, name: 'Item 1' },
-                { id: 2, name: 'Item 2' },
-                { id: 3, name: 'Item 3' }
+            boxes: [
+                { id: 1, x: 50, y: 50, width: 200, height: 150, content: 'Box 1' },
+                { id: 2, x: 300, y: 100, width: 200, height: 150, content: 'Box 2' },
+                { id: 3, x: 600, y: 200, width: 200, height: 150, content: 'Box 3' }
             ],
-            draggedItemIndex: null
+            draggedBoxIndex: null,
+            dragStartX: 0,
+            dragStartY: 0
         };
     },
     mounted() {
-        this.items.forEach((item, index) => {
-            const element = this.$el.querySelectorAll('.draggable')[index];
+        this.boxes.forEach((box, index) => {
+            const element = this.$el.querySelectorAll('.bounding-box')[index];
             draggable({ element });
             dropTargetForElements({ element });
         });
     },
     methods: {
-        onDragStart(index) {
-            this.draggedItemIndex = index;
+        onDragStart(event, index) {
+            this.draggedBoxIndex = index;
+            this.dragStartX = event.clientX - this.boxes[index].x;
+            this.dragStartY = event.clientY - this.boxes[index].y;
         },
-        onDrop(targetIndex) {
-            const draggedItem = this.items[this.draggedItemIndex];
-            this.items.splice(this.draggedItemIndex, 1);
-            this.items.splice(targetIndex, 0, draggedItem);
-            this.draggedItemIndex = null;
+        onDrop(event, targetIndex) {
+            const draggedBox = this.boxes[this.draggedBoxIndex];
+            draggedBox.x = event.clientX - this.dragStartX;
+            draggedBox.y = event.clientY - this.dragStartY;
+            this.draggedBoxIndex = null;
         }
     }
 };
 </script>
-
 <style>
-.draggable {
-    border: 1px solid #ccc;
-    padding: 10px;
-    margin: 5px;
-    background-color: #f9f9f9;
+.canvas {
+    position: relative;
+    width: 3840px;
+    height: 2160px;
+    border: 1px solid #000;
+}
+
+.bounding-box {
+    position: absolute;
+    border: 2px solid #000;
+    background-color: rgba(255, 255, 255, 0.8);
     cursor: move;
-    color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
