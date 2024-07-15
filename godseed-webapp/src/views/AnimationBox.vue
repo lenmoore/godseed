5bff3766c5af3aec0ccc17920f90fe5d21e09b3a
lenmoore <len.vainmaa@gmail.com>
2024-07-15 16:33:59 +0300

    The animation works perfectly. Great success

--- godseed-webapp/src/views/AnimationBox.vue
+++ godseed-webapp/src/views/AnimationBox.vue
@@ -18,10 +18,16 @@ export default {
     data() {
         return {
             currentFrame: [],
+            frameIndex: 1, // Start with the first frame
+            timer: null
         };
     },
     created() {
         this.updateFrame();
+        this.startAnimation();
+    },
+    beforeDestroy() {
+        this.stopAnimation();
     },
     watch: {
         item: {
@@ -32,6 +38,19 @@ export default {
         }
     },
     methods: {
+        startAnimation() {
+            this.timer = setInterval(() => {
+                // Toggle frame index between 1 and 2
+                this.frameIndex = this.frameIndex === 1 ? 2 : 1;
+                this.updateFrame();
+            }, 1000); // Change frame every second
+        },
+        stopAnimation() {
+            if (this.timer) {
+                clearInterval(this.timer);
+                this.timer = null;
+            }
+        },
         updateFrame() {
             const parameters = {
                 dinosaurs_domesticated: JSON.parse(localStorage.getItem('dinosaurs_domesticated')) || false,
@@ -39,16 +58,14 @@ export default {
                 auras_real: JSON.parse(localStorage.getItem('auras_real')) || false
             };
 
-            const frameNumber = 1; // Set the initial frame, you can add logic to change frames over time
-            const frameData = this.item.animationData[frameNumber];
+            const frameData = this.item.animationData[this.frameIndex];
             const baseImages = frameData.base;
-            console.log(baseImages)
             const dinoImages = frameData.dinosaurs_domesticated[parameters.dinosaurs_domesticated];
             const insectImages = frameData.insects_huge[parameters.insects_huge];
             const auraImages = frameData.auras_real[parameters.auras_real];
 
             this.currentFrame = [
-                "bg.png",
+                frameData.bg,
                 ...baseImages,
                 ...dinoImages,
                 ...insectImages,
