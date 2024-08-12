28d09916545ab21abeea64a01e87d5aeba7d5933
lenmoore <len.vainmaa@gmail.com>
2024-08-12 08:55:13 +0300

    log uri

--- godseed-backend/main.js
+++ godseed-backend/main.js
@@ -1,39 +1,40 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
-import parameterRoutes from './src/routes/parameter.routes.js';
-import eraRoutes from './src/routes/era.routes.js';
-import sceneRoutes from './src/routes/scene.routes.js';
-import variationRoutes from './src/routes/variation.routes.js';
-import uploadRoutes from './src/routes/upload.routes.js';  // Import the upload routes
+import parameterRoutes from './src/routes/parameter.routes.js'
+import eraRoutes from './src/routes/era.routes.js'
+import sceneRoutes from './src/routes/scene.routes.js'
+import variationRoutes from './src/routes/variation.routes.js'
+import uploadRoutes from './src/routes/upload.routes.js' // Import the upload routes
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(cors());  // Enable CORS for all origins
-app.use(express.json());
-app.use('/uploads', express.static('uploads')); // To serve uploaded files statically
+app.use(cors())  // Enable CORS for all origins
+app.use(express.json())
+app.use('/uploads', express.static('uploads')) // To serve uploaded files statically
 
+console.log(process.env.MONGODB_URI)
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
   .then(() => console.log('MongoDB connected!'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => console.error('MongoDB connection error:', err))
 
 // Use routes with /api prefix
-app.use('/api/parameters', parameterRoutes);
-app.use('/api/eras', eraRoutes);
-app.use('/api/scenes', sceneRoutes);
-app.use('/api/variations', variationRoutes);
-app.use('/api/upload', uploadRoutes); // Register the upload routes
+app.use('/api/parameters', parameterRoutes)
+app.use('/api/eras', eraRoutes)
+app.use('/api/scenes', sceneRoutes)
+app.use('/api/variations', variationRoutes)
+app.use('/api/upload', uploadRoutes) // Register the upload routes
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
